ee4948e4038a23d45ee84c1290f8f06c9389913b
Hamilton Greene <hamilton.greene@gmail.com>
2016-02-06 15:43:38 -0500

    Added a bunch of functions and nothing's broken, yet nothing works

--- Frontend/scripts/googlemaps.js
+++ Frontend/scripts/googlemaps.js
@@ -14,47 +14,73 @@ function initMap() {
   directionsDisplay.setMap(map);
 
   document.getElementById('submit').addEventListener('click', function() {
-    //calculateAndDisplayRoute(directionsService, directionsDisplay);
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+  });
+}
+
+function httpGetAsync(theUrl, callback)
+{
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() {
+      if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+          callback(xmlHttp.responseText);
+  }
+  xmlHttp.open("GET", theUrl, true); // true for asynchronous
+  xmlHttp.send(req);
+}
+
+//Implement http call to backend here
+function calculateNewRoute(req, __callback) {
+  httpGetAsync("http://localhost:8000/api/update-route", function() {
+
   });
 }
 
 function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-  var waypts = calculateNewRoute();
-  var checkboxArray = document.getElementById('waypoints');
-  for (var i = 0; i < checkboxArray.length; i++) {
-    if (checkboxArray.options[i].selected) {
-      waypts.push({
-        location: checkboxArray[i].value,
-        stopover: true
-      });
-    }
-  }
+  //Need to update this with real latitude and longitude
+  var waypts = [];
+  calculateNewRoute({lat: -34, lng: 20}, function(err, res) {
+    //var checkboxArray = document.getElementById('waypoints');
+    var start;
 
-  directionsService.route({
-    origin: document.getElementById('start').value,
-    destination: document.getElementById('end').value,
-    waypoints: waypts,
-    optimizeWaypoints: true,
-    travelMode: google.maps.TravelMode.DRIVING
-  }, function(response, status) {
-    if (status === google.maps.DirectionsStatus.OK) {
-      directionsDisplay.setDirections(response);
-      var route = response.routes[0];
-      var summaryPanel = document.getElementById('directions-panel');
-      summaryPanel.innerHTML = '';
-      // For each route, display summary information.
-      for (var i = 0; i < route.legs.length; i++) {
-        var routeSegment = i + 1;
-        summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
-            '</b><br>';
-        summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
-        summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
-        summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
-      }
-    } else {
-      window.alert('Directions request failed due to ' + status);
+    //We're only worried about circuits right now, so start == end
+    for (var i = 1; i < res.length; i++) {
+        if(i == 0) start = res.splice(i, 1);
+        else if(i != 0) {
+          waypts.push({
+            location: res.splice(i, 1),
+            stopover: true
+          });
+        }
     }
+
+    directionsService.route({
+      origin: start,
+      destination: start,
+      waypoints: waypts,
+      optimizeWaypoints: true,
+      travelMode: google.maps.TravelMode.WALKING,
+    }, function(response, status) {
+      if (status === google.maps.DirectionsStatus.OK) {
+        directionsDisplay.setDirections(response);
+        var route = response.routes[0];
+        var summaryPanel = document.getElementById('directions-panel');
+        summaryPanel.innerHTML = '';
+        // For each route, display summary information.
+        for (var i = 0; i < route.legs.length; i++) {
+          var routeSegment = i + 1;
+          summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
+              '</b><br>';
+          summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
+          summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
+          summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
+        }
+      } else {
+        window.alert('Directions request failed due to ' + status);
+      }
+    });
   });
+
 }
 
 //Calls for map to be updated, shows new
