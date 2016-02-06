89bc26ab8df86521e239366d854e4a3ed0aaf3d6
Hamilton Greene <hamilton.greene@gmail.com>
2016-02-06 12:41:50 -0500

    Node server runs on localhost 8000, has API hooks to hit

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+//**API JUnk**
+
+var router = express.Router();
+
+
+app.use('/api', router);
+
+app.listen(8000,function(){
+    console.log("Started listening on port", 8000);
+})
